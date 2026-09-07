8b73aecb29c5517ac282446146d8b879b71620bc
PriyaSugu <30488432+PriyaSugu@users.noreply.github.com>
2026-09-07 08:42:57 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        int s1len = s1.length();
+        int[] s1freq = new int[26];
+
+        for(char ch: s1.toCharArray()){
+             s1freq[ch - 'a']++;
+        }
+
+        int left = 0;
+        int[] window = new int[26];
+        for(int right = 0; right < s2.length(); right++){
+            
+            window[s2.charAt(right) - 'a']++;
+            
+            if(right - left + 1 > s1len){
+                window[s2.charAt(left) - 'a']--;
+                left++;
+            }
+            if(right - left + 1 == s1len){
+                boolean found = true;
+                for(int i = 0; i < 26; i++){
+                    if(window[i] != s1freq[i]){
+                        found = false;
+                    }        
+                }
+                if(found){
+                    return true;
+                }
+            }
+            
+        }
+
+        return false;
+
+        
+    }
+}
